4ec9485ff61a34c3bb3c10eb0fb5cf5ef4ca9f1b
Benjamin Herrera <43798089+BenjaminHerrera@users.noreply.github.com>
2021-04-03 15:00:05 -0700

    Redo passing long_description

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup, find_packages
 
-long_description = open('README.text').read() + '\n\n' + open('CHANGELOG.txt').read()
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 classifiers = [
     'Development Status :: 4 - Beta',
